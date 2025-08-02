3d54f66c8e495819425b09bd157bde187f3429d5
elazzi <elazzi@gmail.com>
2025-08-03 01:30:00 +0200

    change

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('archiveButton').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'archive' });
-  });
-});
